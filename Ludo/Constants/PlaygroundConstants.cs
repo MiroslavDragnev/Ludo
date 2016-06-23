536f70396ec8aa453df3a0d4ec57cf67ecace9a8
krasi-totev <krasi.totev@gmail.com>
2016-06-23 11:42:04 +0300

    Adding class Pawn, and renaming constants.

--- Ludo/Constants/PlaygroundConstants.cs
+++ Ludo/Constants/PlaygroundConstants.cs
@@ -8,17 +8,17 @@ namespace Ludo.Constants
 {
     public static class PlaygroundConstants
     {
-        public const int redStartIndex = 0;
-        public const int greenStartIndex = 14;
-        public const int yellowStartIndex = 28;
-        public const int blueStartIndex = 42;
+        public const int RED_START_INDEX = 0;
+        public const int GREEN_START_INDEX = 14;
+        public const int YELLOW_START_INDEX = 28;
+        public const int BLUE_START_INDEX = 42;
 
-        public const int redEntryToFinish = 55;
-        public const int greenEntryToFinish = 13;
-        public const int yellowEntryToFinish = 27;
-        public const int blueEntryToFinish = 41;
+        public const int RED_ENTRY_TO_FINISH = 55;
+        public const int GREEN_ENTRY_TO_FINISH = 13;
+        public const int YELLOW_ENTRY_TO_FINISH = 27;
+        public const int BLUE_ENTRY_TO_FINISH = 41;
 
-        public const int playgroundSize = 56;
-        public const int finishSize = 5;
+        public const int PLAYGROUND_SIZE = 56;
+        public const int FINISH_SIZE = 5;
     }
 }
---
+++ Ludo/Models/Pawn.cs
@@ -0,0 +1,60 @@
+﻿namespace Ludo.Models
+{
+    using Ludo.Enumerations;
+    using Ludo.Constants;
+    public class Pawn
+    {
+        public Pawn(ColorType color)
+        {
+            this.Color = color;
+            this.OnGameField = false;
+            this.Escaped = false;
+            this.SetStartAndEndPossitions();
+        }
+
+        public int StartPossition { get; private set; }
+        public int EntryToFinish { get; private set; }
+        public int Possition { get; private set; }
+        public bool OnGameField { get; set; }
+        public bool Escaped { get; set; }
+        public ColorType Color { get; set; }
+        
+        //Move Pawn
+        public void Move(int diceNumber)
+        {
+            this.Possition += diceNumber;
+            if (this.Color != ColorType.Red && this.Possition >= PlaygroundConstants.PLAYGROUND_SIZE)
+            {
+                this.Possition %= PlaygroundConstants.PLAYGROUND_SIZE;
+            }
+
+            //TODO (escaping the playground)
+            //TODO (reseting possition when send home)
+        }
+
+        //Generate initial possitions
+        private void SetStartAndEndPossitions()
+        {
+            if (this.Color == ColorType.Red)
+            {
+                this.StartPossition = PlaygroundConstants.RED_START_INDEX;
+                this.EntryToFinish = PlaygroundConstants.RED_ENTRY_TO_FINISH;
+            }
+            else if (this.Color == ColorType.Green)
+            {
+                this.StartPossition = PlaygroundConstants.GREEN_START_INDEX;
+                this.EntryToFinish = PlaygroundConstants.GREEN_ENTRY_TO_FINISH;
+            }
+            else if (this.Color == ColorType.Yellow)
+            {
+                this.StartPossition = PlaygroundConstants.YELLOW_START_INDEX;
+                this.EntryToFinish = PlaygroundConstants.YELLOW_ENTRY_TO_FINISH;
+            }
+            else if (this.Color == ColorType.Blue)
+            {
+                this.StartPossition = PlaygroundConstants.BLUE_START_INDEX;
+                this.EntryToFinish = PlaygroundConstants.BLUE_ENTRY_TO_FINISH;
+            }
+        }
+    }
+}
