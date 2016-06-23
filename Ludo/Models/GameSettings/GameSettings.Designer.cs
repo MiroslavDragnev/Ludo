5191bfe6e409cd0b76c2d993c502d8866e1cee33
Miroslav Dragnev <mdragnev96@gmail.com>
2016-06-23 12:56:24 +0300

    Some GUI work

--- Ludo/Models/GameSettings/GameSettings.Designer.cs
+++ Ludo/Models/GameSettings/GameSettings.Designer.cs
@@ -28,12 +28,225 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.plrOneCheck = new System.Windows.Forms.CheckBox();
+            this.plrTwoCheck = new System.Windows.Forms.CheckBox();
+            this.plrThreeCheck = new System.Windows.Forms.CheckBox();
+            this.plrFourCheck = new System.Windows.Forms.CheckBox();
+            this.plrOneText = new System.Windows.Forms.TextBox();
+            this.plrTwoText = new System.Windows.Forms.TextBox();
+            this.plrThreeText = new System.Windows.Forms.TextBox();
+            this.plrFourText = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblWarning = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // plrOneCheck
+            // 
+            this.plrOneCheck.AutoSize = true;
+            this.plrOneCheck.Location = new System.Drawing.Point(12, 12);
+            this.plrOneCheck.Name = "plrOneCheck";
+            this.plrOneCheck.Size = new System.Drawing.Size(64, 17);
+            this.plrOneCheck.TabIndex = 1;
+            this.plrOneCheck.Text = "Player 1";
+            this.plrOneCheck.UseVisualStyleBackColor = true;
+            // 
+            // plrTwoCheck
+            // 
+            this.plrTwoCheck.AutoSize = true;
+            this.plrTwoCheck.Location = new System.Drawing.Point(12, 46);
+            this.plrTwoCheck.Name = "plrTwoCheck";
+            this.plrTwoCheck.Size = new System.Drawing.Size(64, 17);
+            this.plrTwoCheck.TabIndex = 2;
+            this.plrTwoCheck.Text = "Player 2";
+            this.plrTwoCheck.UseVisualStyleBackColor = true;
+            // 
+            // plrThreeCheck
+            // 
+            this.plrThreeCheck.AutoSize = true;
+            this.plrThreeCheck.Location = new System.Drawing.Point(12, 80);
+            this.plrThreeCheck.Name = "plrThreeCheck";
+            this.plrThreeCheck.Size = new System.Drawing.Size(64, 17);
+            this.plrThreeCheck.TabIndex = 3;
+            this.plrThreeCheck.Text = "Player 3";
+            this.plrThreeCheck.UseVisualStyleBackColor = true;
+            // 
+            // plrFourCheck
+            // 
+            this.plrFourCheck.AutoSize = true;
+            this.plrFourCheck.Location = new System.Drawing.Point(12, 113);
+            this.plrFourCheck.Name = "plrFourCheck";
+            this.plrFourCheck.Size = new System.Drawing.Size(64, 17);
+            this.plrFourCheck.TabIndex = 4;
+            this.plrFourCheck.Text = "Player 4";
+            this.plrFourCheck.UseVisualStyleBackColor = true;
+            // 
+            // plrOneText
+            // 
+            this.plrOneText.Location = new System.Drawing.Point(82, 9);
+            this.plrOneText.Name = "plrOneText";
+            this.plrOneText.Size = new System.Drawing.Size(102, 20);
+            this.plrOneText.TabIndex = 5;
+            this.plrOneText.Text = "Enter name";
+            // 
+            // plrTwoText
+            // 
+            this.plrTwoText.Location = new System.Drawing.Point(82, 43);
+            this.plrTwoText.Name = "plrTwoText";
+            this.plrTwoText.Size = new System.Drawing.Size(102, 20);
+            this.plrTwoText.TabIndex = 6;
+            this.plrTwoText.Text = "Enter name";
+            // 
+            // plrThreeText
+            // 
+            this.plrThreeText.Location = new System.Drawing.Point(82, 77);
+            this.plrThreeText.Name = "plrThreeText";
+            this.plrThreeText.Size = new System.Drawing.Size(102, 20);
+            this.plrThreeText.TabIndex = 7;
+            this.plrThreeText.Text = "Enter name";
+            // 
+            // plrFourText
+            // 
+            this.plrFourText.Location = new System.Drawing.Point(82, 110);
+            this.plrFourText.Name = "plrFourText";
+            this.plrFourText.Size = new System.Drawing.Size(102, 20);
+            this.plrFourText.TabIndex = 8;
+            this.plrFourText.Text = "Enter name";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Red;
+            this.pictureBox1.Location = new System.Drawing.Point(201, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Lime;
+            this.pictureBox2.Location = new System.Drawing.Point(201, 43);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Yellow;
+            this.pictureBox3.Location = new System.Drawing.Point(201, 77);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Blue;
+            this.pictureBox4.Location = new System.Drawing.Point(201, 110);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox4.TabIndex = 12;
+            this.pictureBox4.TabStop = false;
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.Location = new System.Drawing.Point(12, 146);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(66, 20);
+            this.btnAbout.TabIndex = 13;
+            this.btnAbout.Text = "About";
+            this.btnAbout.UseVisualStyleBackColor = true;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(82, 146);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(69, 20);
+            this.btnStart.TabIndex = 14;
+            this.btnStart.Text = "Start game";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(156, 146);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(66, 20);
+            this.btnExit.TabIndex = 15;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblWarning
+            // 
+            this.lblWarning.AutoSize = true;
+            this.lblWarning.Location = new System.Drawing.Point(17, 169);
+            this.lblWarning.Name = "lblWarning";
+            this.lblWarning.Size = new System.Drawing.Size(36, 13);
+            this.lblWarning.TabIndex = 16;
+            this.lblWarning.Text = "";
+            // 
+            // GameSettings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(234, 189);
+            this.Controls.Add(this.lblWarning);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnAbout);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.plrFourText);
+            this.Controls.Add(this.plrThreeText);
+            this.Controls.Add(this.plrTwoText);
+            this.Controls.Add(this.plrOneText);
+            this.Controls.Add(this.plrFourCheck);
+            this.Controls.Add(this.plrThreeCheck);
+            this.Controls.Add(this.plrTwoCheck);
+            this.Controls.Add(this.plrOneCheck);
+            this.Name = "GameSettings";
+            this.Text = "Select player count";
+            this.Load += new System.EventHandler(this.GameSettings_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckBox plrOneCheck;
+        private System.Windows.Forms.CheckBox plrTwoCheck;
+        private System.Windows.Forms.CheckBox plrThreeCheck;
+        private System.Windows.Forms.CheckBox plrFourCheck;
+        private System.Windows.Forms.TextBox plrOneText;
+        private System.Windows.Forms.TextBox plrTwoText;
+        private System.Windows.Forms.TextBox plrThreeText;
+        private System.Windows.Forms.TextBox plrFourText;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button btnAbout;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblWarning;
     }
 }
 
--- Ludo/Models/GameSettings/GameSettings.cs
+++ Ludo/Models/GameSettings/GameSettings.cs
@@ -16,5 +16,62 @@ namespace Ludo
         {
             InitializeComponent();
         }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            int players = 0;
+            bool emptyName = false;
+
+            if (plrOneCheck.Checked)
+            {
+                players++;
+
+                if (string.IsNullOrEmpty(plrOneText.Text))
+                    emptyName = true;
+            }
+            if (plrTwoCheck.Checked)
+            {
+                players++;
+
+                if (string.IsNullOrEmpty(plrTwoText.Text))
+                    emptyName = true;
+            }
+            if (plrThreeCheck.Checked)
+            {
+                players++;
+
+                if (string.IsNullOrEmpty(plrThreeText.Text))
+                    emptyName = true;
+            }
+            if (plrFourCheck.Checked)
+            {
+                players++;
+
+                if (string.IsNullOrEmpty(plrFourText.Text))
+                    emptyName = true;
+            }
+
+            if(players < 2)
+            {
+                //lblWarning.Location = new System.Drawing.Point(17, 169);
+                lblWarning.Text = "At least two players have to be checked.";
+            }
+
+            if(emptyName)
+            {
+                //lblWarning.Location = new System.Drawing.Point(17, 169);
+                lblWarning.Text = "Please, don't leave empty name spaces.";
+            }
+        }
+
+        private void GameSettings_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
