1b238f31ef526f85ab4f4faaf00d95c7cca81677
MiroslavDragnev <mdragnev96@gmail.com>
2016-06-24 14:16:07 +0300

    Renamed img to Resources

--- Ludo/Models/GameSettings/GameSettings.Designer.cs
+++ Ludo/Models/GameSettings/GameSettings.Designer.cs
@@ -56,10 +56,10 @@ namespace Ludo
             // plrOneCheck
             // 
             this.plrOneCheck.AutoSize = true;
-            this.plrOneCheck.ForeColor = SystemColors.ButtonHighlight;
-            this.plrOneCheck.Location = new Point(12, 12);
+            this.plrOneCheck.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.plrOneCheck.Location = new System.Drawing.Point(12, 12);
             this.plrOneCheck.Name = "plrOneCheck";
-            this.plrOneCheck.Size = new Size(64, 17);
+            this.plrOneCheck.Size = new System.Drawing.Size(64, 17);
             this.plrOneCheck.TabIndex = 1;
             this.plrOneCheck.Text = "Player 1";
             this.plrOneCheck.UseVisualStyleBackColor = true;
@@ -67,10 +67,10 @@ namespace Ludo
             // plrTwoCheck
             // 
             this.plrTwoCheck.AutoSize = true;
-            this.plrTwoCheck.ForeColor = SystemColors.ButtonHighlight;
-            this.plrTwoCheck.Location = new Point(12, 46);
+            this.plrTwoCheck.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.plrTwoCheck.Location = new System.Drawing.Point(12, 46);
             this.plrTwoCheck.Name = "plrTwoCheck";
-            this.plrTwoCheck.Size = new Size(64, 17);
+            this.plrTwoCheck.Size = new System.Drawing.Size(64, 17);
             this.plrTwoCheck.TabIndex = 2;
             this.plrTwoCheck.Text = "Player 2";
             this.plrTwoCheck.UseVisualStyleBackColor = true;
@@ -78,10 +78,10 @@ namespace Ludo
             // plrThreeCheck
             // 
             this.plrThreeCheck.AutoSize = true;
-            this.plrThreeCheck.ForeColor = SystemColors.ButtonHighlight;
-            this.plrThreeCheck.Location = new Point(12, 80);
+            this.plrThreeCheck.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.plrThreeCheck.Location = new System.Drawing.Point(12, 80);
             this.plrThreeCheck.Name = "plrThreeCheck";
-            this.plrThreeCheck.Size = new Size(64, 17);
+            this.plrThreeCheck.Size = new System.Drawing.Size(64, 17);
             this.plrThreeCheck.TabIndex = 3;
             this.plrThreeCheck.Text = "Player 3";
             this.plrThreeCheck.UseVisualStyleBackColor = true;
@@ -89,118 +89,118 @@ namespace Ludo
             // plrFourCheck
             // 
             this.plrFourCheck.AutoSize = true;
-            this.plrFourCheck.ForeColor = SystemColors.ButtonHighlight;
-            this.plrFourCheck.Location = new Point(12, 113);
+            this.plrFourCheck.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.plrFourCheck.Location = new System.Drawing.Point(12, 113);
             this.plrFourCheck.Name = "plrFourCheck";
-            this.plrFourCheck.Size = new Size(64, 17);
+            this.plrFourCheck.Size = new System.Drawing.Size(64, 17);
             this.plrFourCheck.TabIndex = 4;
             this.plrFourCheck.Text = "Player 4";
             this.plrFourCheck.UseVisualStyleBackColor = true;
             // 
             // plrOneText
             // 
-            this.plrOneText.Location = new Point(82, 9);
+            this.plrOneText.Location = new System.Drawing.Point(82, 9);
             this.plrOneText.Name = "plrOneText";
-            this.plrOneText.Size = new Size(102, 20);
+            this.plrOneText.Size = new System.Drawing.Size(102, 20);
             this.plrOneText.TabIndex = 5;
             this.plrOneText.Text = "Metallica";
             // 
             // plrTwoText
             // 
-            this.plrTwoText.Location = new Point(82, 43);
+            this.plrTwoText.Location = new System.Drawing.Point(82, 43);
             this.plrTwoText.Name = "plrTwoText";
-            this.plrTwoText.Size = new Size(102, 20);
+            this.plrTwoText.Size = new System.Drawing.Size(102, 20);
             this.plrTwoText.TabIndex = 6;
             this.plrTwoText.Text = "Motorhead";
             // 
             // plrThreeText
             // 
-            this.plrThreeText.Location = new Point(82, 77);
+            this.plrThreeText.Location = new System.Drawing.Point(82, 77);
             this.plrThreeText.Name = "plrThreeText";
-            this.plrThreeText.Size = new Size(102, 20);
+            this.plrThreeText.Size = new System.Drawing.Size(102, 20);
             this.plrThreeText.TabIndex = 7;
             this.plrThreeText.Text = "Megadeth";
             // 
             // plrFourText
             // 
-            this.plrFourText.Location = new Point(82, 110);
+            this.plrFourText.Location = new System.Drawing.Point(82, 110);
             this.plrFourText.Name = "plrFourText";
-            this.plrFourText.Size = new Size(102, 20);
+            this.plrFourText.Size = new System.Drawing.Size(102, 20);
             this.plrFourText.TabIndex = 8;
             this.plrFourText.Text = "Iron Maiden";
             // 
             // pictureBox1
             // 
-            this.pictureBox1.BackColor = Color.Red;
-            this.pictureBox1.Location = new Point(201, 9);
+            this.pictureBox1.BackColor = System.Drawing.Color.Red;
+            this.pictureBox1.Location = new System.Drawing.Point(201, 9);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new Size(20, 20);
+            this.pictureBox1.Size = new System.Drawing.Size(20, 20);
             this.pictureBox1.TabIndex = 9;
             this.pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
-            this.pictureBox2.BackColor = Color.Lime;
-            this.pictureBox2.Location = new Point(201, 43);
+            this.pictureBox2.BackColor = System.Drawing.Color.Lime;
+            this.pictureBox2.Location = new System.Drawing.Point(201, 43);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new Size(20, 20);
+            this.pictureBox2.Size = new System.Drawing.Size(20, 20);
             this.pictureBox2.TabIndex = 10;
             this.pictureBox2.TabStop = false;
             // 
             // pictureBox3
             // 
-            this.pictureBox3.BackColor = Color.Yellow;
-            this.pictureBox3.Location = new Point(201, 77);
+            this.pictureBox3.BackColor = System.Drawing.Color.Yellow;
+            this.pictureBox3.Location = new System.Drawing.Point(201, 77);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new Size(20, 20);
+            this.pictureBox3.Size = new System.Drawing.Size(20, 20);
             this.pictureBox3.TabIndex = 11;
             this.pictureBox3.TabStop = false;
             // 
             // pictureBox4
             // 
-            this.pictureBox4.BackColor = Color.Blue;
-            this.pictureBox4.Location = new Point(201, 110);
+            this.pictureBox4.BackColor = System.Drawing.Color.Blue;
+            this.pictureBox4.Location = new System.Drawing.Point(201, 110);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new Size(20, 20);
+            this.pictureBox4.Size = new System.Drawing.Size(20, 20);
             this.pictureBox4.TabIndex = 12;
             this.pictureBox4.TabStop = false;
             // 
             // btnAbout
             // 
-            this.btnAbout.BackColor = Color.Transparent;
-            this.btnAbout.BackgroundImage = ((Image)(resources.GetObject("btnAbout.BackgroundImage")));
+            this.btnAbout.BackColor = System.Drawing.Color.Transparent;
+            this.btnAbout.BackgroundImage = global::Ludo.Properties.Resources.About_button;
             this.btnAbout.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.btnAbout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnAbout.Location = new Point(12, 146);
+            this.btnAbout.Location = new System.Drawing.Point(12, 146);
             this.btnAbout.Name = "btnAbout";
-            this.btnAbout.Size = new Size(66, 22);
+            this.btnAbout.Size = new System.Drawing.Size(66, 22);
             this.btnAbout.TabIndex = 13;
             this.btnAbout.UseVisualStyleBackColor = false;
             this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click);
             // 
             // btnStart
             // 
-            this.btnStart.BackColor = Color.Transparent;
-            this.btnStart.BackgroundImage = ((Image)(resources.GetObject("btnStart.BackgroundImage")));
+            this.btnStart.BackColor = System.Drawing.Color.Transparent;
+            this.btnStart.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnStart.BackgroundImage")));
             this.btnStart.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.btnStart.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            this.btnStart.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnStart.Location = new Point(82, 146);
+            this.btnStart.Location = new System.Drawing.Point(82, 146);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new Size(69, 22);
+            this.btnStart.Size = new System.Drawing.Size(69, 22);
             this.btnStart.TabIndex = 14;
             this.btnStart.UseVisualStyleBackColor = false;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
             // btnExit
             // 
-            this.btnExit.BackColor = Color.Transparent;
-            this.btnExit.BackgroundImage = ((Image)(resources.GetObject("btnExit.BackgroundImage")));
+            this.btnExit.BackColor = System.Drawing.Color.Transparent;
+            this.btnExit.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnExit.BackgroundImage")));
             this.btnExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnExit.Location = new Point(156, 146);
+            this.btnExit.Location = new System.Drawing.Point(156, 146);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new Size(66, 22);
+            this.btnExit.Size = new System.Drawing.Size(66, 22);
             this.btnExit.TabIndex = 15;
             this.btnExit.UseVisualStyleBackColor = false;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
@@ -208,18 +208,18 @@ namespace Ludo
             // lblWarning
             // 
             this.lblWarning.AutoSize = true;
-            this.lblWarning.Location = new Point(17, 172);
+            this.lblWarning.ForeColor = System.Drawing.Color.Red;
+            this.lblWarning.Location = new System.Drawing.Point(17, 172);
             this.lblWarning.Name = "lblWarning";
-            this.lblWarning.Size = new Size(0, 13);
+            this.lblWarning.Size = new System.Drawing.Size(0, 13);
             this.lblWarning.TabIndex = 16;
-            this.lblWarning.ForeColor = Color.Red;
             // 
             // GameSettings
             // 
-            this.AutoScaleDimensions = new SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = SystemColors.ActiveCaptionText;
-            this.ClientSize = new Size(234, 193);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(234, 193);
             this.Controls.Add(this.lblWarning);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnStart);
@@ -236,7 +236,7 @@ namespace Ludo
             this.Controls.Add(this.plrThreeCheck);
             this.Controls.Add(this.plrTwoCheck);
             this.Controls.Add(this.plrOneCheck);
-            this.Icon = ((Icon)(resources.GetObject("$this.Icon")));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "GameSettings";
             this.Text = "Select player count";
             this.Load += new System.EventHandler(this.GameSettings_Load);
