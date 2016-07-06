e225693bb05aff07a1e515b690a4bed273491e73
Ivan <madritistaaa@gmail.com>
2016-07-06 22:22:48 +0300

    Change player status to sleeping

--- Ludo/Models/Tokens/SleepToken.cs
+++ Ludo/Models/Tokens/SleepToken.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ludo.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace Ludo.Models.Tokens
 {
-    class SleepToken
+    public class SleepToken : Token
     {
+        public override void DoAction(Player player, Pawn pawn)
+        {
+            player.IsSleeping = true;
+        }
     }
 }
