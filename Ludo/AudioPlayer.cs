77b0bb8568e25776c3357c4f19f2a36528c3770c
krasi-totev <krasi.totev@gmail.com>
2016-07-09 17:16:06 +0300

    refactoring usings ina AudioPlayer

---
+++ Ludo/AudioPlayer.cs
@@ -0,0 +1,18 @@
+﻿namespace Ludo
+{
+    using System.Media;
+    public static class AudioPlayer
+    {
+        public static SoundPlayer player = new SoundPlayer();
+        public static void PlayClickSound()
+        {
+            player.SoundLocation = @"../../Sounds/Click.wav";
+            player.Play();
+        }
+        public static void PlayHoverSound()
+        {
+            player.SoundLocation = @"../../Sounds/Hover.wav";
+            player.Play();
+        }
+    }
+}
